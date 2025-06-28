32183140c2510a2707d52c2e9fc40d299c5c3ffe
pkra99 <pkra99bux>
2025-06-28 21:47:13 +0530

    adjust cors

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: "32kb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://to-do-app-blush-omega.vercel.app/',
     credentials: true
 }))
 app.use(rateLimiter)
